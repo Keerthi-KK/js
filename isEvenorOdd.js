b7e095e3122348bbb8dc57f5b9ea467f08ac46d1
Keerthi-KK <kkeerthikanagaraj@gmail.com>
2023-03-21 21:22:10 +0530

    commit 2

---
+++ isEvenorOdd.js
@@ -0,0 +1,13 @@
+//this code will iterate from zero to 15 and checks if odd or even
+
+for (var x=0; x<=15; x++) {
+    if (x === 0) {
+            console.log(x +  " is even");
+    }
+    else if (x % 2 === 0) {
+            console.log(x + " is even");   
+    }
+    else {
+            console.log(x + " is odd");
+    }
+}
